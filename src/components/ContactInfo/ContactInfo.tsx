380e5f8ad635b801bfa14a517881ec47708d7302
sanchopedro <pedro_sancho@yahoo.com.br>
2023-07-26 19:32:18 +0100

    update img problem in nextjs

--- src/components/ContactInfo/ContactInfo.tsx
+++ src/components/ContactInfo/ContactInfo.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Mail from "/images/Mail.svg";
+import Image from "next/image";
 
 function ContactInfo() {
   return (
@@ -8,28 +8,25 @@ function ContactInfo() {
         <li>
           <h6 className="text-gray-400 font-semibold">Phone</h6>
           <div className="flex space-x-2 mt-2 text-gray-50">
-            <img src="/images/whatsapp.svg" alt="whatsapp logo" />
+            <Image
+              src="/images/whatsapp.svg"
+              alt="whatsapp logo"
+              width={24}
+              height={24}
+            />
             <span className="text-sm">+351 911 060 590</span>
           </div>
         </li>
         <li>
           <h6 className="text-gray-400 font-semibold">Email</h6>
           <div className="flex space-x-2 mt-2 text-gray-50">
-            <svg
-              className="w-5 "
-              fill="none"
-              stroke="#3B82F6"
-              strokeWidth={2}
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
-              />
-            </svg>
+            <Image
+              src="/images/icon-mail.svg"
+              alt="Mail logo"
+              
+              width={24}
+              height={24}
+            />
             <span className="text-sm">pedro.sancho17@gmail.com</span>
           </div>
         </li>
@@ -37,16 +34,31 @@ function ContactInfo() {
           <h6 className="text-gray-400 font-semibold">Social Profiles</h6>
           <div className="flex space-x-4 mt-2 text-gray-50">
             <a href="https://github.com/sanchopedro" target="_blank">
-              <img src="/images/icon-github.svg" alt="whatsapp logo" />
+              <Image
+                src="/images/icon-github.svg"
+                alt="github logo"
+                width={24}
+                height={24}
+              />
             </a>
             <a href="https://www.instagram.com/sanchopedro/" target="_blank">
-              <img src="/images/icon-instagram.svg" alt="Instagram logo" />
+              <Image
+                src="/images/icon-instagram.svg"
+                alt="instagram logo"
+                width={24}
+                height={24}
+              />
             </a>
             <a
               href="https://www.linkedin.com/in/pedrosanchorodrigues/"
               target="_blank"
             >
-              <img src="/images/icon-linkedin.svg" alt="Linkedin logo" />
+              <Image
+                src="/images/icon-linkedin.svg"
+                alt="icon-linkedin logo"
+                width={24}
+                height={24}
+              />
             </a>
           </div>
         </li>
