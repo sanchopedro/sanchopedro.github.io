6c3f0b7bbea6efa773b35484d05c8e1496bb9e64
sanchopedro <pedro_sancho@yahoo.com.br>
2023-08-02 22:29:15 +0100

    Refactor(Header): code refactoring

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,109 +1,76 @@
 import Image from "next/image";
 
 function Header() {
+  const menuItems = [
+    { text: "About Me", icon: "user", link: "./about" },
+    { text: "Portfolio", icon: "folder", link: "./portfolio" },
+    { text: "Contact Me", icon: "phone", link: "./contact" },
+  ];
+
   return (
     <nav className="container flex justify-between items-center px-4 py-8 mx-auto">
       <div>
-          <a href="./">
-            <Image
-              src="/images/logo_white.svg"
-              alt="Minha Imagem"
-              width={250}
-              height={250}
-            ></Image>
-          </a>
+        <a href="./">
+          <Image
+            src="/images/logo_white.svg"
+            alt="Sancho Dev Logo"
+            width={250}
+            height={250}
+          />
+        </a>
       </div>
       <div className="hidden space-x-8 md:flex">
-        <ul className="flex items-center ml-auto md:space-x-4 lg:space-x-8 uppercase text-white ">
-          <li className="flex items-center">
-            <svg
-              className="w-8 h-4 mr-2"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="1.5"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-              ></path>
-            </svg>
-            <a
-              href="./about"
-              className="md:text-md lg:text-xl  hover:text-gray-300 hover:underline"
-            >
-              About Me
-            </a>
-          </li>
-          <li className="flex items-center">
-            <svg
-              className="w-4 h-4 mr-2"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="1.5"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z"
-              ></path>
-            </svg>
-            <a
-              href="./portfolio"
-              className="md:text-md lg:text-xl  hover:text-gray-300 hover:underline"
-            >
-              Portfolio
-            </a>
-          </li>
-          <li className="flex items-center">
-            <svg
-              className="w-4 h-4 mr-2"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="1.5"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
-              ></path>
-            </svg>
-            <a
-              href="./contact"
-              className="md:text-md lg:text-xl  hover:text-gray-300 hover:underline"
-            >
-              Contact Me
-            </a>
-          </li>
+        <ul className="flex items-center ml-auto md:space-x-4 lg:space-x-8 uppercase text-white">
+          {menuItems.map((item) => (
+            <li className="flex items-center" key={item.text}>
+              <svg
+                className="w-8 h-4 mr-2"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d={
+                    item.icon === "user"
+                      ? "M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                      : item.icon === "folder"
+                      ? "M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z"
+                      : "M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+                  }
+                ></path>
+              </svg>
+              <a
+                href={item.link}
+                className="md:text-md lg:text-xl  hover:text-gray-300 hover:underline"
+              >
+                {item.text}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
       <div className="flex md:hidden">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="w-6 h-6"
+          className="w-8 h-8"
           fill="none"
           viewBox="0 0 24 24"
           stroke="#AAABAB"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="M4 6h16M4 12h16M4 18h16"
           />
         </svg>
       </div>
     </nav>
-
   );
 }
 
